e903e088717c7b9f673929521570a1626c4f8b98
Leperte <lapertosareis@gmail.com>
2026-04-22 13:39:15 -0300

    Implement diceroll function to simulate dice rolls

---
+++ diceroll.js
@@ -0,0 +1,19 @@
+function diceroll() {
+    let results = [];
+    for (let i = 0; i < 4; i++) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        results.push(dice);
+    }
+    
+    results.sort((a, b) => b - a)
+    
+    let lowest = results[3];
+    
+    return {
+        rolls: results,
+        dropped: lowest,
+        total: results.reduce((sum, val) => sum + val, 0)
+    };
+}
+
+console.log(diceroll());

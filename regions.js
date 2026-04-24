f66e4404f46fb3b301931e7001b1f76866aa9d93
Leperte <lapertosareis@gmail.com>
2026-04-24 15:18:18 -0300

    Update regions.js

--- regions.js
+++ regions.js
@@ -1,7 +1,21 @@
 const regions = [
    { 
-    name: "Toran",
+    name: "Toran - Grandes Nações",
     sub: ["Império Tazuriano", "República de Taldoria", "República de Velanor", "Reino de Aniria"]
+  },
+   { 
+    name: "Toran - Nações Livres",
+    sub: ["Ilhas do Farol", "Ilhas da Passagem", "Ilhas de Doran", "Ilhas da Fratura"]
+  },
+   { 
+    name: "Alara",
+    sub: ["Plano Elemental do Fogo", "Plano Elemental do Ar", "Plano Elemental da Terra ", "Plano Elemental da Água", "Plano Féerico", "Plano das Sombras"]
   },
   { 
 ]
+
+  const randomRegion = regions[Math.floor(Math.random() * regions.length)];
+  const randomSubregion = randomRegion.subregion[Math.floor(Math.random() * randomRegiom.subregion.length)
+];
+
+console.log(randomRegion.name + " - " + randomSubregion);
